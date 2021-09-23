c6d77624ab5c6b6ce173697388e168c18d74d63c
tatiana-albin <87941420+tatiana-albin@users.noreply.github.com>
2021-09-23 21:26:58 +0300

    Delete src directory

--- src/main/java/com/p5/adoptions/repository/shelters/AnimalShelterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.p5.adoptions.repository.shelters;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnimalShelterRepository extends JpaRepository<AnimalShelter, Integer>
-{
-}
--- src/main/java/com/p5/adoptions/service/exceptions/ShelterLocationException.java
+++
@@ -1,8 +0,0 @@
-package com.p5.adoptions.service.exceptions;
-
-public class ShelterLocationException extends RuntimeException {
-
-    public ShelterLocationException(String message) {
-        super(message);
-    }
-}
